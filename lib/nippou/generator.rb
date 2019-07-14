235537137feb5038d68e458b716cc1a639b7b836
Ryo SUETSUGU <suetsugu.r@gmail.com>
2019-07-14 11:09:18 +0900

    init

---
+++ lib/nippou/generator.rb
@@ -0,0 +1,8 @@
+require "nippou/generator/version"
+
+module Nippou
+  module Generator
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/nippou/generator_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Nippou::Generator do
+  it "has a version number" do
+    expect(Nippou::Generator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
